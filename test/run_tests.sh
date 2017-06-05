3cc65353f9658380161946e2a6e24462ef19e477
MarcosPividori <marcos.pividori@gmail.com>
2017-06-05 13:23:51 -0300

    red color for failing tests.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,3 +1,5 @@
+RED='\033[0;31m'
+NC='\033[0m'
 for TEST in $(find . -name *.tig)
 do
   if grep -q "EXPECTED:" $TEST
@@ -5,7 +7,7 @@ do
     CHECK=`sed -n 's/\/\*\s*EXPECTED:\s*"\([^"]*\)".*/\1/p' $TEST`
     if ../build/tiger $TEST 2>&1 | grep -q "$CHECK"
       then echo "TEST $TEST: PASS"
-      else echo "TEST $TEST: FAIL (Expected: \"$CHECK\")"
+      else echo "${RED}TEST $TEST: FAIL (Expected: \"$CHECK\")${NC}"
     fi
   fi
 done
