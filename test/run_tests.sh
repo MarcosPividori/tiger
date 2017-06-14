e91ecd1f72e2526c2d42ffd8e6838dcbcc15bc05
MarcosPividori <marcos.pividori@gmail.com>
2017-06-14 13:06:34 -0300

    improve test!

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,14 +1,30 @@
 RED='\033[0;31m'
 NC='\033[0m'
+
+export LIBRARY_PATH=../build:$LIBRARY_PATH
+
 for TEST in $(find . -name *.tig)
 do
-  if grep -q "EXPECTED:" $TEST
-  then
-    CHECK=`sed -n 's/\/\*\s*EXPECTED:\s*"\([^"]*\)".*/\1/p' $TEST`
-    echo -n "TEST $TEST: "
+  grep "EXPECTED-COMP:" $TEST | while read -r line; do
+    CHECK=`echo $line | sed -n 's/[^"]*"\([^"]*\)".*/\1/p'`
+    echo -n "TEST-COMP $TEST: "
     if ../build/tiger $TEST 2>&1 | grep -q "$CHECK"
       then echo "PASS"
       else echo "${RED}FAIL (Expected: \"$CHECK\")${NC}"
     fi
-  fi
+  done
+
+  if grep -q "EXPECTED-RUN:" $TEST
+  then ../build/tiger $TEST > /dev/null 2>&1; fi
+
+  grep "EXPECTED-RUN:" $TEST | while read -r line; do
+    RUN=`echo $line | sed -n 's/[^"]*"\([^"]*\)".*/\1/p'`
+    CHECK=`echo $line | sed -n 's/[^"]*"[^"]*"\s*=>\s*"\([^"]*\)".*/\1/p'`
+    echo -n "TEST-RUN $TEST: "
+    if /bin/sh -c "$RUN" 2>&1 | grep -q "$CHECK"
+      then echo "PASS"
+      else echo "${RED}FAIL (Cmd: \"$RUN\" Expected: \"$CHECK\")${NC}"
+    fi
+  done
+
 done
