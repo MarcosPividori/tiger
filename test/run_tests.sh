67cc3f76954ce6de2c9b1016761ec400c483ccad
MarcosPividori <marcos.pividori@gmail.com>
2017-06-17 17:01:19 -0300

    improve shell script for tests.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,6 +5,7 @@ export LIBRARY_PATH=../build:$LIBRARY_PATH
 
 for TEST in $(find . -name *.tig)
 do
+  # Execute the compilation tests.
   grep "EXPECTED-COMP:" $TEST | while read -r line; do
     CHECK=`echo $line | sed -n 's/[^"]*"\([^"]*\)".*/\1/p'`
     echo -n "TEST-COMP $TEST: "
@@ -14,9 +15,11 @@ do
     fi
   done
 
-  if grep -q "EXPECTED-RUN:" $TEST
+  # If necessary, build the executable for the run tests.
+  if grep -q "EXPECTED-RUN:" $TEST && ! grep -q "EXPECTED-COMP:" $TEST
   then ../build/tiger $TEST > /dev/null 2>&1; fi
 
+  # Execute the run tests.
   grep "EXPECTED-RUN:" $TEST | while read -r line; do
     RUN=`echo $line | sed -n 's/[^"]*"\([^"]*\)".*/\1/p'`
     CHECK=`echo $line | sed -n 's/[^"]*"[^"]*"\s*=>\s*"\([^"]*\)".*/\1/p'`
